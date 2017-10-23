752d794c8cc4a06eaac6ad4ae76c7bb98bcc27ce
xiewe <xiewe9@163.com>
2017-10-23 19:27:03 +0800

    commit

--- src/main/java/com/framework/concurrency/ExecutorServiceManage.java
+++ src/main/java/com/framework/concurrency/ExecutorServiceManage.java
@@ -31,4 +31,7 @@ public class ExecutorServiceManage {
 		ExecutorServiceManage.scheduledExecutorService = scheduledExecutorService;
 	}
 
+    public void shutDown(){
+        executeService.shutdown();
+    }
 }
